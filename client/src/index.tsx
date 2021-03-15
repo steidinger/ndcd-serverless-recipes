8814bc0c6644b1bf8e15b9ec008e51a43085b411
Frank Steidinger <steidinger@acm.org>
2021-03-15 19:05:39 +0100

    chore: cleanup index.html, disable mock service worker

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './mocks/setup-worker';
+// import './mocks/setup-worker';
 
 ReactDOM.render(
   <React.StrictMode>
